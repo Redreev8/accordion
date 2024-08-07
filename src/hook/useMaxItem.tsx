84ad9305dcedada2c174f61159b958ebf281035e
Redreev8 <156510412+Redreev8@users.noreply.github.com>
2024-08-08 03:58:20 +0500

    rename items to itemsActive

--- src/hook/useMaxItem.tsx
+++ src/hook/useMaxItem.tsx
@@ -8,17 +8,17 @@ export type UseMaxItemReturn<T = JSX.Element> = {
 }
 
 const useMaxItem = <T = JSX.Element>(max: number = 1) : UseMaxItemReturn<T> => {
-    const [items, setItems] = useState<T[]>([])
+    const [itemsActive, setItemsActive] = useState<T[]>([])
     if (max === 0) return { itemsActive: [], push: () => {}, remove: () => {} }
     const push = (r: T) => {
         if (max === 0) return
-        setItems(prev => [...prev, r])
-        if (items.length >= max) remove(items[items.length - 1])
+        setItemsActive(prev => [...prev, r])
+        if (itemsActive.length >= max) remove(itemsActive[itemsActive.length - 1])
     }
     const remove = (r: T) => {
-        setItems(prev => prev.filter(el => el !== r))
+        setItemsActive(prev => prev.filter(el => el !== r))
     }
-    return { itemsActive: items, push, remove }
+    return { itemsActive, push, remove }
 }
 
 export default useMaxItem
